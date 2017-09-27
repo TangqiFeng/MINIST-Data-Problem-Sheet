43faf44bff7ead0e8f9f81712ab831225a482115
Kyle Feng <kylezete@gmail.com>
2017-09-27 20:07:15 +0100

    output images to files

--- outputImageFile.py
+++ outputImageFile.py
@@ -4,9 +4,25 @@
 
 import PIL.Image as pil
 import outputImageToConsole as out
-myimage = pil.fromarray(out.np.array(out.readDataFile.train_images[3]))
-mylabel = out.readDataFile.train_labels[3]
-# convert image
-myimage = myimage.convert('RGB')
-myimage.show()
-myimage.save('images/train-0003-1.png')
+def outImages(type):
+    if(type == 'test'):
+        for i in range(len(out.readDataFile.test_images)):
+            myimage = pil.fromarray(out.np.array(out.readDataFile.test_images[i]))
+            mylabel = out.readDataFile.test_labels[i]
+            # convert image
+            myimage = myimage.convert('RGB')
+            # save image
+            imagename = 'test_images/test-'+str(i)+'-'+str(mylabel)+'.png'
+            myimage.save(imagename)
+    if(type == 'train'):
+        for i in range(len(out.readDataFile.train_images)):
+            myimage = pil.fromarray(out.np.array(out.readDataFile.train_images[i]))
+            mylabel = out.readDataFile.train_labels[i]
+            # convert image
+            myimage = myimage.convert('RGB')
+            # save image
+            imagename = 'train_images/train-'+str(i)+'-'+str(mylabel)+'.png'
+            myimage.save(imagename)
+
+outImages('test')
+outImages('train')
