c241c677057f9641ad7652acc328cec082447045
Kyle Feng <kylezete@gmail.com>
2017-09-27 14:36:27 +0100

    save a image

--- outputImageFile.py
+++ outputImageFile.py
@@ -1,7 +1,12 @@
 # references:
-#   PIL.Images:  http://effbot.org/imagingbook/image.htm
+#   PIL.Images(fromarray, convert, save etc.):
+#   http://effbot.org/imagingbook/image.htm
 
 import PIL.Image as pil
 import outputImageToConsole as out
 myimage = pil.fromarray(out.np.array(out.readDataFile.train_images[3]))
+mylabel = out.readDataFile.train_labels[3]
+# convert image
+myimage = myimage.convert('RGB')
 myimage.show()
+myimage.save('images/train-0003-1.png')
